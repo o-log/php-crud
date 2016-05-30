b564fe597ec1cd0c7107132c3c33e3c7a60d16a7
mkozlov1 <650465@gmail.com>
2016-05-30 20:38:54 +0300

    1

---
+++ OLOG/CRUD/CRUDTableWidgetTimestamp.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace OLOG\CRUD;
+
+use OLOG\Sanitize;
+
+class CRUDTableWidgetTimestamp implements InterfaceCRUDTableWidget
+{
+    protected $timestamp;
+    protected $format;
+
+    /**
+     * Returns sanitized content.
+     * @param $obj
+     * @return mixed
+     */
+    public function html($obj)
+    {
+        $timestamp = CRUDCompiler::compile($this->getTimestamp(), ['this' => $obj]);
+        $date = date($this->getFormat(), $timestamp);
+        return Sanitize::sanitizeTagContent($date);
+    }
+
+    public function __construct($timestamp, $format = "Y-m-d H:i:s")
+    {
+        $this->setTimestamp($timestamp);
+        $this->setFormat($format);
+    }
+
+    /**
+     * @return int
+     */
+    public function getTimestamp()
+    {
+        return $this->timestamp;
+    }
+
+    /**
+     * @param int $timestamp
+     */
+    public function setTimestamp($timestamp)
+    {
+        $this->timestamp = $timestamp;
+    }
+
+    public function getFormat()
+    {
+        return $this->format;
+    }
+
+    public function setFormat($format)
+    {
+        $this->format = $format;
+    }
+}
