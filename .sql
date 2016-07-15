5db60e312ce20764a37cb03f2db11dce13ef3843
Arthur <zeusakm@gmail.com>
2016-07-15 15:33:14 +0300

    t-fix-null minor fixes

--- .sql
+++
@@ -1,3 +0,0 @@
-array(
-'alter table node add column gender tinyint(1)   default 1  /* rand955234 */;',
-)
--- CRUDDemo/DemoTerm.php
+++ CRUDDemo/DemoTerm.php
@@ -3,107 +3,106 @@
 namespace CRUDDemo;
 
 class DemoTerm implements
-	\OLOG\Model\InterfaceFactory,
-	\OLOG\Model\InterfaceLoad,
-	\OLOG\Model\InterfaceSave,
-	\OLOG\Model\InterfaceDelete
+    \OLOG\Model\InterfaceFactory,
+    \OLOG\Model\InterfaceLoad,
+    \OLOG\Model\InterfaceSave,
+    \OLOG\Model\InterfaceDelete
 {
-	use \OLOG\Model\FactoryTrait;
-	use \OLOG\Model\ActiveRecord;
-	use \OLOG\Model\ProtectProperties;
+    use \OLOG\Model\FactoryTrait;
+    use \OLOG\Model\ActiveRecord;
+    use \OLOG\Model\ProtectProperties;
 
-	const DB_ID         = 'phpcrud';
-	const DB_TABLE_NAME = 'term';
+    const DB_ID         = 'phpcrud';
+    const DB_TABLE_NAME = 'term';
 
-	protected $chooser = null;
-	protected $options = null;
-	protected $id;
+    protected $chooser = null;
+    protected $options = null;
+    protected $id;
+    protected $title   = '';
+    protected $gender  = null;
+    protected $parent_id;
 
-	public function getOptions()
-	{
-		return $this->options;
-	}
+    public function getOptions()
+    {
+        return $this->options;
+    }
 
-	public function setOptions($value)
-	{
-		$this->options = $value;
-	}
+    public function setOptions($value)
+    {
+        $this->options = $value;
+    }
 
-	public function getChooser()
-	{
-		return $this->chooser;
-	}
+    public function getChooser()
+    {
+        return $this->chooser;
+    }
 
-	public function setChooser($value)
-	{
-		$this->chooser = $value;
-	}
+    public function setChooser($value)
+    {
+        $this->chooser = $value;
+    }
 
-	protected $title  = '';
-	protected $gender = null;
-	protected $parent_id;
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
 
-	/**
-	 * @return int
-	 */
-	public function getId()
-	{
-		return $this->id;
-	}
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
 
-	/**
-	 * @param int $id
-	 */
-	public function setId($id)
-	{
-		$this->id = $id;
-	}
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getTitle()
-	{
-		return $this->title;
-	}
+    /**
+     * @param string $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
 
-	/**
-	 * @param string $title
-	 */
-	public function setTitle($title)
-	{
-		$this->title = $title;
-	}
+    /**
+     * @return string
+     */
+    public function getGender()
+    {
+        return $this->gender;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getGender()
-	{
-		return $this->gender;
-	}
+    /**
+     * @param string $gender
+     */
+    public function setGender($gender)
+    {
+        $this->gender = $gender;
+    }
 
-	/**
-	 * @param string $gender
-	 */
-	public function setGender($gender)
-	{
-		$this->gender = $gender;
-	}
+    /**
+     * @return mixed
+     */
+    public function getParentId()
+    {
+        return $this->parent_id;
+    }
 
-	/**
-	 * @return mixed
-	 */
-	public function getParentId()
-	{
-		return $this->parent_id;
-	}
-
-	/**
-	 * @param mixed $parent_id
-	 */
-	public function setParentId($parent_id)
-	{
-		$this->parent_id = $parent_id;
-	}
+    /**
+     * @param mixed $parent_id
+     */
+    public function setParentId($parent_id)
+    {
+        $this->parent_id = $parent_id;
+    }
 }
