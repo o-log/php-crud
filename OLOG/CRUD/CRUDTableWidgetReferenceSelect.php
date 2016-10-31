104ed22774db118354c5fd524b07c54870b2e430
mkozlov1 <650465@gmail.com>
2016-10-31 16:11:05 +0300

    1

--- OLOG/CRUD/CRUDTableWidgetReferenceSelect.php
+++ OLOG/CRUD/CRUDTableWidgetReferenceSelect.php
@@ -3,27 +3,36 @@
 namespace OLOG\CRUD;
 
 use OLOG\Assert;
-use OLOG\Operations;
 use OLOG\Sanitize;
 
 class CRUDTableWidgetReferenceSelect implements InterfaceCRUDTableWidget
 {
     protected $title_field_name;
+    protected $id_field_name;
 
-    public function __construct($title_field_name)
+    public function __construct($title_field_name, $id_field_name = '')
     {
         $this->setTitleFieldName($title_field_name);
+        $this->setIdFieldName($id_field_name);
     }
 
-    public function html($obj){
+    public function html($obj)
+    {
         Assert::assert($obj);
 
         $title_field_name = $this->getTitleFieldName();
 
         $obj_title = CRUDFieldsAccess::getObjectFieldValue($obj, $title_field_name);
 
+        $id_field_name = $this->getIdFieldName();
+        if($id_field_name == '') {
+            $id = CRUDFieldsAccess::getObjId($obj);
+        } else {
+            $id = CRUDFieldsAccess::getObjectFieldValue($obj, $id_field_name);
+        }
+
         $o = '';
-        $o .='<button class="btn btn-xs btn-default js-ajax-form-select" type="submit" data-id="' . Sanitize::sanitizeAttrValue(CRUDFieldsAccess::getObjId($obj)) . '" data-title="' . Sanitize::sanitizeAttrValue($obj_title) . '">Выбор</button>';
+        $o .= '<button class="btn btn-xs btn-default js-ajax-form-select" type="submit" data-id="' . Sanitize::sanitizeAttrValue($id) . '" data-title="' . Sanitize::sanitizeAttrValue($obj_title) . '">Выбор</button>';
 
         return $o;
     }
@@ -44,5 +53,19 @@ class CRUDTableWidgetReferenceSelect implements InterfaceCRUDTableWidget
         $this->title_field_name = $title_field_name;
     }
 
+    /**
+     * @return mixed
+     */
+    public function getIdFieldName()
+    {
+        return $this->id_field_name;
+    }
 
+    /**
+     * @param mixed $id_field_name
+     */
+    public function setIdFieldName($id_field_name)
+    {
+        $this->id_field_name = $id_field_name;
+    }
 }
