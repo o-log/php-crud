e25bbcea27db4daad1db7fa5311b016aa1d64cde
Антон Пинегин <pinegin.anton@gmail.com>
2016-11-02 15:42:49 +0300

    Fix

--- OLOG/CRUD/CRUDFormWidgetDateTime.php
+++ OLOG/CRUD/CRUDFormWidgetDateTime.php
@@ -6,161 +6,161 @@ use OLOG\Sanitize;
 
 class CRUDFormWidgetDateTime implements InterfaceCRUDFormWidget
 {
-    protected $field_name;
-    protected $options_arr;
-    protected $show_null_checkbox;
-    protected $is_required;
+	protected $field_name;
+	protected $show_null_checkbox;
+	protected $is_required;
 
-    public function __construct($field_name, $show_null_checkbox = false, $is_required = false)
-    {
-        $this->setFieldName($field_name);
-        $this->setShowNullCheckbox($show_null_checkbox);
-        $this->setIsRequired($is_required);
-    }
+	public function __construct($field_name, $show_null_checkbox = false, $is_required = false)
+	{
+		$this->setFieldName($field_name);
+		$this->setShowNullCheckbox($show_null_checkbox);
+		$this->setIsRequired($is_required);
+	}
 
-    public function html($obj)
-    {
-        static $CRUDFormWidgetDateTime_include_script;
+	public function html($obj)
+	{
+		static $CRUDFormWidgetDateTime_include_script;
 
-        $field_name = $this->getFieldName();
-        $field_value = CRUDFieldsAccess::getObjectFieldValue($obj, $field_name);
+		$field_name = $this->getFieldName();
+		$field_value = CRUDFieldsAccess::getObjectFieldValue($obj, $field_name);
 
-        /* Нужно изменить на нах CDN */
-        $script = '';
-        if (!isset($CRUDFormWidgetDateTime_include_script)) {
-            $script = '
-								<script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.12.0/moment.min.js"></script>
-								<script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.12.0/locale/ru.js"></script>
-				<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/css/bootstrap-datetimepicker.min.css">
-								<script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/js/bootstrap-datetimepicker.min.js"></script>
+		/* Нужно изменить на нах CDN */
+		$script = '';
+		if (!isset($CRUDFormWidgetDateTime_include_script)) {
+			$script = '
+								<script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.15.2/moment.min.js"></script>
+								<script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.15.2/locale/ru.js"></script>
+				<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.43/css/bootstrap-datetimepicker.min.css">
+								<script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.43/js/bootstrap-datetimepicker.min.js"></script>
 			';
-            $CRUDFormWidgetDateTime_include_script = false;
-        }
+			$CRUDFormWidgetDateTime_include_script = false;
+		}
 
-        $is_required_str = '';
-        if ($this->is_required) {
-            $is_required_str = ' required ';
-        }
+		$is_required_str = '';
+		if ($this->is_required) {
+			$is_required_str = ' required ';
+		}
 
-        $field_value_attr = '';
-        if ($field_value) {
-            $field_value_attr = date('d-m-Y H:i:s', strtotime($field_value));
-        }
+		$field_value_attr = '';
+		if ($field_value) {
+			$field_value_attr = date('d-m-Y H:i:s', strtotime($field_value));
+		}
 
-        $uniqid = uniqid('CRUDFormWidgetDateTime_');
-        $input_cols = $this->getShowNullCheckbox() ? '10' : '12';
+		$uniqid = uniqid('CRUDFormWidgetDateTime_');
+		$input_cols = $this->getShowNullCheckbox() ? '10' : '12';
 
-        $html = '';
-        $html .= '<div class="row">';
-        $html .= '<div class="col-sm-' . $input_cols . '">';
+		$html = '';
+		$html .= '<div class="row">';
+		$html .= '<div class="col-sm-' . $input_cols . '">';
 
-        ob_start(); ?>
-            <input type="hidden" id="<?= $uniqid ?>_input" name="<?= Sanitize::sanitizeAttrValue($field_name) ?>" value="<?= Sanitize::sanitizeTagContent($field_value) ?>" data-field="<?= $uniqid ?>_date" <?= $is_required_str ?>>
-            <div class="input-group date" id="<?= $uniqid ?>">
-                <input id="<?= $uniqid ?>_date" type="text" class="form-control" value="<?= $field_value_attr ?>">
-                <span class="input-group-addon">
+		ob_start(); ?>
+		<input type="hidden" id="<?= $uniqid ?>_input" name="<?= Sanitize::sanitizeAttrValue($field_name) ?>" value="<?= Sanitize::sanitizeTagContent($field_value) ?>" data-field="<?= $uniqid ?>_date" <?= $is_required_str ?>>
+		<div class="input-group date" id="<?= $uniqid ?>">
+			<input id="<?= $uniqid ?>_date" type="text" class="form-control" value="<?= $field_value_attr ?>">
+			<span class="input-group-addon">
                     <span class="glyphicon glyphicon-calendar"></span>
                 </span>
-            </div>
-            <script>
-            $("#<?= $uniqid ?>").datetimepicker({
-                format: "DD-MM-YYYY HH:mm:ss",
-                sideBySide: true,
-                showTodayButton: true
-            }).on("dp.change", function(obj){
-                if (obj.date) {
-                    $("#<?= $uniqid ?>_input").val(obj.date.format("YYYY-MM-DD HH:mm:ss")).trigger('change');
-                }
-            });
-            </script>
-        <?php
-        $html .= ob_get_clean();
-        $html .= '</div>';
+		</div>
+		<script>
+			$("#<?= $uniqid ?>").datetimepicker({
+				format: "DD-MM-YYYY HH:mm:ss",
+				sideBySide: true,
+				showTodayButton: true
+			}).on("dp.change", function (obj) {
+				if (obj.date) {
+					$("#<?= $uniqid ?>_input").val(obj.date.format("YYYY-MM-DD HH:mm:ss")).trigger('change');
+				} else {
+					$("#<?= $uniqid ?>_input").val('').trigger('change');
+				}
+			});
+		</script>
+		<?php
+		$html .= ob_get_clean();
+		$html .= '</div>';
 
-        if ($this->getShowNullCheckbox()) {
+		if ($this->getShowNullCheckbox()) {
 
-            $is_null_checked = '';
-            if (is_null($field_value)) {
-                $is_null_checked = ' checked ';
-            }
-            ob_start(); ?>
-            <div class="col-sm-2">
-                <label class="form-control-static">
-                    <input id="<?= $uniqid ?>___is_null" type="checkbox" value="1" name="<?= Sanitize::sanitizeAttrValue($field_name) ?>___is_null" <?= $is_null_checked ?>> NULL
-                </label>
-            </div>
-            <script>
-                (function () {
-                    var $input_is_null = $('#<?= $uniqid ?>___is_null');
+			$is_null_checked = '';
+			if (is_null($field_value)) {
+				$is_null_checked = ' checked ';
+			}
+			ob_start(); ?>
+			<div class="col-sm-2">
+				<label class="form-control-static">
+					<input id="<?= $uniqid ?>___is_null" type="checkbox" value="1" name="<?= Sanitize::sanitizeAttrValue($field_name) ?>___is_null" <?= $is_null_checked ?>> NULL
+				</label>
+			</div>
+			<script>
+				(function () {
+					var $input_is_null = $('#<?= $uniqid ?>___is_null');
 
-                    $("#<?= $uniqid ?>_input").on('change', function () {
-                        $input_is_null.prop('checked',false);
-                    });
+					$("#<?= $uniqid ?>_input").on('change', function () {
+						$input_is_null.prop('checked',false);
+					});
 
-                    $input_is_null.on('change', function () {
-                        if ($(this).is(':checked')) {
-                            $('#<?= $uniqid ?>').data("DateTimePicker").clear();
-                            $('#<?= $uniqid ?>_input').val('');
-                        }
-                    });
-                })();
-            </script>
-            <?php
-            $html .= ob_get_clean();
-        }
+					$input_is_null.on('change', function () {
+						if ($(this).is(':checked')) {
+							$('#<?= $uniqid ?>').data("DateTimePicker").clear();
+							$('#<?= $uniqid ?>_input').val('');
+						}
+					});
+				})();
+			</script>
+			<?php
+			$html .= ob_get_clean();
+		}
 
-        $html .= '</div>';
+		$html .= '</div>';
 
-        return $script . $html;
-    }
+		return $script . $html;
+	}
 
-    /**
-     * @return mixed
-     */
-    public function getFieldName()
-    {
-        return $this->field_name;
-    }
+	/**
+	 * @return mixed
+	 */
+	public function getFieldName()
+	{
+		return $this->field_name;
+	}
 
-    /**
-     * @param mixed $field_name
-     */
-    public function setFieldName($field_name)
-    {
-        $this->field_name = $field_name;
-    }
+	/**
+	 * @param mixed $field_name
+	 */
+	public function setFieldName($field_name)
+	{
+		$this->field_name = $field_name;
+	}
 
-    /**
-     * @return mixed
-     */
-    public function getIsRequired()
-    {
-        return $this->is_required;
-    }
+	/**
+	 * @return mixed
+	 */
+	public function getIsRequired()
+	{
+		return $this->is_required;
+	}
 
-    /**
-     * @param mixed $is_required
-     */
-    public function setIsRequired($is_required)
-    {
-        $this->is_required = $is_required;
-    }
+	/**
+	 * @param mixed $is_required
+	 */
+	public function setIsRequired($is_required)
+	{
+		$this->is_required = $is_required;
+	}
 
-    /**
-     * @return mixed
-     */
-    public function getShowNullCheckbox()
-    {
-        return $this->show_null_checkbox;
-    }
+	/**
+	 * @return mixed
+	 */
+	public function getShowNullCheckbox()
+	{
+		return $this->show_null_checkbox;
+	}
 
-    /**
-     * @param mixed $show_null_checkbox
-     */
-    public function setShowNullCheckbox($show_null_checkbox)
-    {
-        $this->show_null_checkbox = $show_null_checkbox;
-    }
+	/**
+	 * @param mixed $show_null_checkbox
+	 */
+	public function setShowNullCheckbox($show_null_checkbox)
+	{
+		$this->show_null_checkbox = $show_null_checkbox;
+	}
 
 }
-
