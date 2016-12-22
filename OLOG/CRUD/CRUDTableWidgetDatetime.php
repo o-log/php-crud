4173fe5590d40883a7a6c478e2bb94965d33f3b0
Антон Пинегин <pinegin.anton@gmail.com>
2016-12-22 14:49:40 +0300

    Fix

---
+++ OLOG/CRUD/CRUDTableWidgetDatetime.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace OLOG\CRUD;
+
+use OLOG\Sanitize;
+
+class CRUDTableWidgetDatetime implements InterfaceCRUDTableWidget
+{
+	protected $datetime;
+	protected $format;
+
+	/**
+	 * Returns sanitized content.
+	 * @param $obj
+	 * @return mixed
+	 */
+	public function html($obj)
+	{
+		$datetime = CRUDCompiler::compile($this->getDatetime(), ['this' => $obj]);
+		$date_obj = new \DateTime($datetime);
+		$date = $date_obj->format($this->getFormat());
+		return Sanitize::sanitizeTagContent($date);
+	}
+
+	public function __construct($datetame, $format = "d.m.Y H:i:s")
+	{
+		$this->setDatetime($datetame);
+		$this->setFormat($format);
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getDatetime()
+	{
+		return $this->datetime;
+	}
+
+	/**
+	 * @param int $timestamp
+	 */
+	public function setDatetime($datetame)
+	{
+		$this->datetime = $datetame;
+	}
+
+	public function getFormat()
+	{
+		return $this->format;
+	}
+
+	public function setFormat($format)
+	{
+		$this->format = $format;
+	}
+}
